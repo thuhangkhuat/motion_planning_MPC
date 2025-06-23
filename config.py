f29acc38b007d79f1115b68eeea0817897a000ea
hangktt <hangktt@phenikaa-x.com>
2025-06-23 17:25:31 +0700

    modify formation

--- config.py
+++ config.py
@@ -57,42 +57,42 @@ if SCENARIO == 1:
     GOALS = STARTS + np.array([21., 0., 0.])
     
     # Obstacle x, y, r
-    # OBSTACLES = np.array([[ 7.0, 2.5, 0.8],
-    #                       [ 7.0, 6.0, 0.8],
-    #                       [ 7.0, 9.0, 0.8],
-    #                       [ 9.0, 1.5, 0.8],
-    #                       [12.0, 4.0, 0.8],
-    #                       [12.0, 7.5, 0.8],
-    #                       [15.0, 1.5, 0.8],
-    #                       [15.0, 6.0, 0.8],
-    #                       [17.0, 9.0, 0.8],
-    #                       [19.0, 3.5, 0.8],
-    #                       [19.0, 6.5, 0.8]])
-    OBSTACLES = np.array([])
+    OBSTACLES = np.array([[ 7.0, 2.5, 0.8],
+                          [ 7.0, 6.5, 0.8],
+                          [ 7.0, 9.0, 0.8],
+                          [ 9.0, 1.5, 0.8],
+                          [12.0, 3.7, 0.8],
+                          [12.0, 7.5, 0.8],
+                          [15.0, 1.5, 0.8],
+                          [15.0, 6.0, 0.8],
+                          [17.0, 9.0, 0.8],
+                          [19.0, 3.5, 0.8],
+                          [19.0, 6.5, 0.8]])
+    # OBSTACLES = np.array([])
     XLIM = [0, 25]
     YLIM = [0, 10]
 elif SCENARIO == 2:
     #Parameters of target
     TAR_MAX_SPEED = 0.8
     # TAR_WAYPOINTS = [np.array([3.0, 5.0, 5.0]),np.array([10.0, 7.0, 5.0]),
-    #                  np.array([15.0, 5.0, 5.0]),np.array([21.0, 5.0, 5.0])]
-    TAR_WAYPOINTS = [np.array([3.0, 2.0, 5.0]),np.array([21.0, 7.0, 5.0])]
+    #                  np.array([15.0, 3.0, 5.0]),np.array([21.0, 5.0, 5.0])]
+    TAR_WAYPOINTS = [np.array([3.0, 5.0, 5.0]),np.array([21.0, 5.0, 5.0])]
     TAR_EPSILON = 0.1
 
     # Parameters of the environment
     VIEWING_RADIUS = 2.5    # R_view: radius of the viewing area
-    DESIRED_SEPARATION = 1.0   # desired distance between robots
+    DESIRED_SEPARATION = 1.5   # desired distance between robots
     OFFSET_SEPARATION = 0.5    # offset distance between robots
     # Parameters of FOV
     HFOV = 60.0             # Horizontal field of view
     VFOV = 80.0             # Vertical field of view
 
     # Weights for MPC
-    W_tra = 0.5
+    W_tra = 5.0
     W_u = 4e-1
-    W_col = 1.5
+    W_col = 1
     W_slack = 100.0
-    W_form_dist = 50.0
+    W_form_dist = 2.0
     W_form_struct = 0.0
 
     # Weights for CBF
@@ -103,23 +103,23 @@ elif SCENARIO == 2:
                     #    [1., 6.5, 3.],
                     #    [1., 7., 3.]])
     NUM_ROBOT = STARTS.shape[0]
-    FORMATION_OFFSETS = generate_circular_formation(NUM_ROBOT, VIEWING_RADIUS - 1, arc_angle_deg=90)
+    FORMATION_OFFSETS = generate_circular_formation(NUM_ROBOT, VIEWING_RADIUS -1 , arc_angle_deg=60)
 
     GOALS = STARTS + np.array([21., 0., 0.])
     
     # Obstacle x, y, r
-    # OBSTACLES = np.array([[ 7.0, 2.5, 0.8],
-    #                       [ 7.0, 6.0, 0.8],
-    #                       [ 7.0, 9.0, 0.8],
-    #                       [ 9.0, 1.5, 0.8],
-    #                       [12.0, 4.0, 0.8],
-    #                       [12.0, 7.5, 0.8],
-    #                       [15.0, 1.5, 0.8],
-    #                       [15.0, 6.0, 0.8],
-    #                       [17.0, 9.0, 0.8],
-    #                       [19.0, 3.5, 0.8],
-    #                       [19.0, 6.5, 0.8]])
-    OBSTACLES = np.array([])
+    OBSTACLES = np.array([[ 7.0, 2.7, 0.8],
+                          [ 7.0, 6.9, 0.8],
+                          [ 7.0, 9.0, 0.8],
+                          [ 9.0, 1.5, 0.8],
+                          [12.0, 3.5, 0.8],
+                          [12.0, 7.5, 0.8],
+                          [15.0, 1.5, 0.8],
+                          [15.0, 6.4, 0.8],
+                          [17.0, 9.0, 0.8],
+                          [19.0, 3.2, 0.8],
+                          [19.0, 6.6, 0.8]])
+    # OBSTACLES = np.array([])
     XLIM = [0, 25]
     YLIM = [0, 10]
 elif SCENARIO == 3:
--- formation.py
+++ formation.py
@@ -15,16 +15,22 @@ def generate_circular_formation(num_drones, radius, arc_angle_deg=180):
     if num_drones == 0:
             return np.array([])
     offsets = []
-    arc_angle_rad = np.deg2rad(arc_angle_deg)
-    start_angle = -arc_angle_rad / 2.0
+    leader_offset = [0.0, 0.0, 0.0]
+    offsets.append(leader_offset)
     
     if num_drones == 1:
-        angle_step = 0
-        start_angle = 0
+        return np.array(offsets)
+    
+    num_followers = num_drones - 1
+    arc_angle_rad = np.deg2rad(arc_angle_deg)
+    start_angle_rad = -arc_angle_rad / 2.0
+    if num_followers == 1:
+        angle_step_rad = 0
+        start_angle_rad = 0 
     else:
-        angle_step = arc_angle_rad / (num_drones - 1)
-    for i in range(num_drones):
-        angle = start_angle + i * angle_step
+        angle_step_rad = arc_angle_rad / (num_followers - 1)
+    for i in range(num_followers):
+        angle = start_angle_rad + i * angle_step_rad
         
         offset_x = radius * np.cos(angle)
         offset_y = radius * np.sin(angle)
